6c26b9237a2474d1e367e6f9e9b014523ad6e66c
Gaurav Sangewar <70481952+techrider27@users.noreply.github.com>
2021-12-23 16:47:14 +0530

    initial-commit

---
+++ take_screenshot_using_python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 28 21:04:09 2021
+
+@author: lenovo
+"""
+import pyautogui
+
+mySc = pyautogui.screenshot()
+# location where we want to save the screenshot
+# kindly edit the location
+mySc.save(r'C:\Users\lenovo\Desktop\relive-react\pic.jpg')
